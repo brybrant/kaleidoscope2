2fae0de121b752dd3fb405772695170a3d2ebe15
Matt Bryant <brybrant@gmail.com>
2026-06-29 15:43:48 +0100

    Fixed vite config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import viteConfig from '@brybrant/vite-config';
 import threeMinifyPlugin from 'rollup-plugin-three-minify';
 
 export default viteConfig({
-  base: '/kaleidoscope2/',
+  base: '/kaleidoscope/',
   plugins: [
     threeMinifyPlugin({
       features: ['clipping', 'colorspace', 'vertices'],
